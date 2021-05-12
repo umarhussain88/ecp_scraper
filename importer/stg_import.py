d3d0b0240e96f3651405475f1cc3ae7d64da3be3
Umar Hussain <umar.hussain.da@outlook.com>
2021-05-12 01:43:52 +0100

    adding changes to find .gz files

--- importer/stg_import.py
+++ importer/stg_import.py
@@ -13,11 +13,12 @@ from sqlalchemy.exc import ProgrammingError
 file_dt = pd.Timestamp('now').strftime('%Y_%m_%d-%H%M%S')
 log_path = Path(__file__).parent.parent.joinpath('logs',f'{file_dt}_export.log')
 
-logging.basicConfig(filename=log_path, 
-                    filemode='w', 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
-                    datefmt='%d-%b-%y %H:%M:%S',
-                    level=logging.INFO)
+logging.basicConfig(
+                filename=log_path,
+                filemode='w',
+                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                datefmt='%d-%b-%y %H:%M:%S',
+                level=logging.INFO)
 
 
 def import_data():
@@ -27,16 +28,17 @@ def import_data():
     sql_user = os.environ.get('sql_uid')
     sql_pw = os.environ.get('sql_pw')
 
-    params = urllib.parse.quote_plus(r'Driver={ODBC Driver 17 for SQL Server};'
-                                    + fr'Server={sql_server}'
-                                    + r',1433;Database=ecp;'
-                                    + fr'Uid={sql_user};Pwd={sql_pw};'
-                                    + fr'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+    params = urllib.parse.quote_plus(
+        r'Driver={ODBC Driver 17 for SQL Server};'
+        + fr'Server={sql_server}'
+        + r',1433;Database=ecp;'
+        + fr'Uid={sql_user};Pwd={sql_pw};'
+        + r'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 
     conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-    engine_azure = create_engine(conn_str, fast_executemany = True)
+    engine_azure = create_engine(conn_str, fast_executemany=True)
 
-    files = Path(__file__).parent.parent.joinpath('output').glob('*.csv')
+    files = Path(__file__).parent.parent.joinpath('output').glob('*.csv*')
 
     df = pd.concat([pd.read_csv(f) for f in files])
 
