959e945ff1845577908830dc078436b62dba8996
Umar Hussain <umar.hussain.da@outlook.com>
2021-05-12 01:22:13 +0100

    adding changes locally for app to reduce file size using gzip and increase logging performance

--- importer/import.py
+++ importer/import.py
@@ -4,23 +4,22 @@ from pathlib import Path
 import json
 
 
-def create_tabular_files(src_path : Optional[str] = 'output') -> None:
+def create_tabular_files(src_path: Optional[str] = 'output') -> None:
 
     files = Path(__file__).parent.parent.joinpath(src_path).glob('*.json')
 
     for file in files:
-        with open(file,'r') as f:
+        with open(file, 'r') as f:
             j = json.load(f)
             df = pd.json_normalize(j)
             df['src'] = file
-            df['extractiondate'] = pd.to_datetime(file.stem.rstrip('_parts'),format='%Y-%m-%dT%H-%M-%S')
-            trg_path = Path(file.parent).joinpath(f"{file.stem}.csv")
-            df.to_csv(trg_path,index=False)
+            df['extractiondate'] = pd.to_datetime(file.stem.rstrip('_parts'),
+                                                  format='%Y-%m-%dT%H-%M-%S')
+            trg_path = Path(file.parent).joinpath(f"{file.stem}.csv.gz")
+            df.to_csv(trg_path, index=False, compression='gzip')
             # remove src json file, no need for this anymore.
             file.unlink()
 
+
 if __name__ == '__main__':
     create_tabular_files()
-
-
-
--- importer/stg_import.py
+++ importer/stg_import.py
@@ -24,27 +24,27 @@ def import_data():
     key_path = Path(__file__).parent.joinpath('key.csv')
     key = pd.read_csv(key_path)['key'][0]
     sql_server = os.environ.get('sql_server')
-    sql_user   = os.environ.get('sql_uid')
-    sql_pw     = os.environ.get('sql_pw')
+    sql_user = os.environ.get('sql_uid')
+    sql_pw = os.environ.get('sql_pw')
 
-
-    params = urllib.parse.quote_plus(r'Driver={ODBC Driver 17 for SQL Server};' 
-                                    + fr'Server={sql_server}' 
-                                    + r',1433;Database=ecp;' 
-                                    + fr'Uid={sql_user};Pwd={sql_pw};' 
+    params = urllib.parse.quote_plus(r'Driver={ODBC Driver 17 for SQL Server};'
+                                    + fr'Server={sql_server}'
+                                    + r',1433;Database=ecp;'
+                                    + fr'Uid={sql_user};Pwd={sql_pw};'
                                     + fr'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 
     conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-    engine_azure = create_engine(conn_str,fast_executemany = True)
+    engine_azure = create_engine(conn_str, fast_executemany = True)
 
     files = Path(__file__).parent.parent.joinpath('output').glob('*.csv')
 
     df = pd.concat([pd.read_csv(f) for f in files])
 
-    logging.info('Writing to Azure DB')
+    logging.info(f'Writing {[f.stem for f in files]} to Azure DB')
 
     try:
-        df.to_sql('product_data',schema='stg_ecp',con=engine_azure,if_exists='replace',index=False)
+        df.to_sql('product_data', schema='stg_ecp', con=engine_azure,
+                  if_exists='replace', index=False)
     except ProgrammingError:
         ip = get('https://api.ipify.org').text
         logging.fatal(f'Program failed due to firewall rule mistach current IP address is - {ip}')
@@ -56,10 +56,11 @@ def import_data():
         logging.info(f'Executing fact ProductPrices with {key}')
         conn.execute('stg_ecp.p_InsertDwFactProductPrices ?', [str(key)])
 
-    key += 1 
+    key += 1
     logging.info(f'updating key with value {key}')
-    pd.DataFrame({'key' : key},index=[0]).to_csv(key_path,index=False)
+    pd.DataFrame({'key': key}, index=[0]).to_csv(key_path, index=False)
 
 
 if __name__ == '__main__':
-    import_data()    
+    import_data()
+    
